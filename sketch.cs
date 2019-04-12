401416c2141f8d62c8ad756c8c69efec7b54a6da
Romy <49555500+Romy02@users.noreply.github.com>
2019-04-12 12:25:22 +0200

    Create sketch.cs

---
+++ sketch.cs
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(700, 700);
+}
+
+function draw() {
+  background(50);
+  noStroke();
+  fill(153, 0, 0);
+  stroke(255);
+  strokeWeight(4);
+  ellipse(60, 60, 110, 110);
+  fill(204, 0, 0)
+  ellipse(120, 60, 110, 110);
+  fill(255, 0, 0)
+  ellipse(60, 120, 110, 110);
+  fill(255, 51, 51)
+  ellipse(120, 120, 110, 110);
+  rect(20, 190, 70, 90)
+  fill(204, 0, 0)
+  rect(60, 190, 70, 90)
+
+
+}
