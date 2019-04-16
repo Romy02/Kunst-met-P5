1a5d37cfaa092e44f50fbd7310a4466d0a2b526a
Romy <49555500+Romy02@users.noreply.github.com>
2019-04-16 15:16:34 +0200

    rode strook

--- sketch.cs
+++ sketch.cs
@@ -1,23 +1,57 @@
 function setup() {
   createCanvas(700, 700);
 }
-
-function draw() {
+function draw (rood) {
   background(50);
   noStroke();
-  fill(153, 0, 0);
-  stroke(255);
-  strokeWeight(4);
-  ellipse(60, 60, 110, 110);
-  fill(204, 0, 0)
-  ellipse(120, 60, 110, 110);
+  fill(153,0,0);
+ stroke(255);
+    strokeWeight(4);
+    ellipse(60, 60, 110, 110);
+  fill(204,0,0)
+    ellipse(120, 60, 110, 110);
   fill(255, 0, 0)
-  ellipse(60, 120, 110, 110);
+    ellipse(60, 120, 110, 110);
   fill(255, 51, 51)
-  ellipse(120, 120, 110, 110);
-  rect(20, 190, 70, 90)
-  fill(204, 0, 0)
-  rect(60, 190, 70, 90)
+    ellipse(120, 120, 110, 110);
+  fill(153,0,0)
+  ellipse(120, 200, 110, 110);
+  fill(204,0,0)
+  ellipse(60, 190, 110, 110);
+fill(255,0,0)
+  ellipse(60,250,110,110);
+  fill(255,51,51)
+  ellipse(120, 270,110,110);
+  fill(153,0,0);
+ellipse(60,300,110,110);
+fill(204,0,0)
+  ellipse(120,340,110,110);
+  fill(255,0,0)
+  ellipse(60,360,110,110);
+  fill(255,51,51)
+  ellipse(120,400,110,110);
+  fill(153,0,0)
+  ellipse(60,430,110,110);
+  fill(204,0,0)
+  ellipse(120,460,110,110);
+  fill(255,0,0)
+  ellipse(60,490,110,110);
+  fill(255,51,51)
+  ellipse(120,520,110,110);
+  fill(153,0,0)
+  ellipse(60,550,110,110);
+  fill(204,0,0)
+  ellipse(120,580,110,110);
+  fill(255,0,0)
+  ellipse(60,610,110,110);
+  fill(255,51,51)
+  ellipse(120,640,110,110);
+  fill(153,0,0)
+  ellipse(60,670,110,100);
+  fill(204,0,0)
+  ellipse(120,700,110,110);
 
-
-}
+          }
+ 
+  
+  
